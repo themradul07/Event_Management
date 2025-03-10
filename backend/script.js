6943704434b9cfb804c8421b4987bc9942e9c40c
MradulGandhi <gandhisir80@gmail.com>
2025-03-10 13:33:05 +0530

    origin

--- backend/script.js
+++ backend/script.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(cors({
-    origin: "http://localhost:5173",  // Allow frontend origin
+    origin: "*",  // Allow frontend origin
     credentials: true                  // Allow sending cookies
 }));
 app.use(express.static(path.join(__dirname,"public")))
