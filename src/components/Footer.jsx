6405f835bc219470d4f81723708def197039a7b1
MradulGandhi <gandhisir80@gmail.com>
2025-03-13 09:09:21 +0530

    Footer fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,12 +3,19 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='w-full h-[30vh] text-white flex flex-col gap-2 justify-center items-center bg-black'>
-        <div>
+        <div className='max-w-[90vw] flex flex-col gap-2 justify-center items-center text-center'
+        >
+           
+           <div >
             Copyright@BIETHUB 2025
-        </div>
-        <div className='font-light'>
+
+           </div>
+<div className='font-light'> 
+
           The website does not have any refund or cancellation policy.
+</div>
         </div>
+       
     </div>
   )
 }
